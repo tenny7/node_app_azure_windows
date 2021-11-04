10383bbb10f065a0074b4d221378a4138dc505fd
Ovie Tennyson <atomictenny@gmail.com>
2021-11-04 00:11:43 +0000

    Addedd counter

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Card from './components/Card'
 function App() {
   
   const [title, setTitle] = useState('Apple Card')
+
+  // counter initialization
+  const [count, setCount] = useState(0)
   const buttonClicked = () => {
     setTitle('Gift Card')
   }
-   
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +22,15 @@ function App() {
         </button>  
         <hr/>
         <Card activeTab={title}/>  
+        <hr/>
+        <button onClick={() => setCount(count + 1)}>Increase Counter</button>
+        <hr/>
+        <button onClick={() => setCount(count - 1)}>Decrease Counter</button>
+
+        <h2>{count}</h2>
       </header>
+
+      
     </div>
   );
 }
